6a4fdddaad8eae0e92566ac29975698b8d13c2ed
berjo <berjodev@gmail.com>
2025-01-08 09:50:03 -0800

    wouter setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { Link, Route, Switch } from "wouter";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <div>
+      <Switch>
+      <Route path="/" component={() => <h1>1</h1>} />
+
+      <Route path="/:id">
+        {(params) => <>Hello, {params.id}!</>}
+      </Route>
+
+      {/* Default route in a switch */}
+      <Route>404: No such page!</Route>
+    </Switch>
+    </div>
   )
 }
 
